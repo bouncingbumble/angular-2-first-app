2a5e633432089e37a38404bea9a7e743ec156644
elongatedmuskrat <jordboudreau@gmail.com>
2018-03-10 21:47:16 -0800

    update componenet with input

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-dashboard',
 	template: `
-		<h1>{{loadState}}</h1>
+		<h1 [textContent]="loadState"></h1>
 		<input type="text" [(ngModel)]="loadState">
 		<button (click)="onUserClick()">Click Me!</button>
 	`
